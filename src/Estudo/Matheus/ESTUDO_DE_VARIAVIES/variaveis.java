0cf155f33873b68bd26003b4abc2037c86660437
matheusxdbr12 <matheusxp21@gmail.com>
2023-01-26 15:54:04 -0300

    CORREÇÃO DO ESTUDO DE VARIAVEIS

---
+++ src/Estudo/Matheus/ESTUDO_DE_VARIAVIES/variaveis.java
@@ -0,0 +1,22 @@
+package Estudo.Matheus.ESTUDO_DE_VARIAVIES;
+
+public class variaveis{     
+  
+public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2800;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+         
+        int numero = 1;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        
+    }
+}
+
