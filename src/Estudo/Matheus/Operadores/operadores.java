bc243a10a8025dc33a196b7fd7dd727c8486aab8
matheusxdbr12 <matheusxp21@gmail.com>
2023-01-27 10:01:19 -0300

    Teste de incremento

--- src/Estudo/Matheus/Operadores/operadores.java
+++ src/Estudo/Matheus/Operadores/operadores.java
@@ -3,18 +3,13 @@ package Estudo.Matheus.Operadores;
 public class operadores {
     public static void main(String[] args) {
         int numero = 5;
+     
+        numero ++;
+        // (numero = numero +1)
+
+        System.out.println(++ numero);
         
-        numero = - numero;
-               
-        System.out.println(numero);
-
-        numero = numero * -1;
-
-        System.out.println(numero);
-
-        numero = ++ numero;
-
-        System.out.println(numero);
+    
                 
     }
     
