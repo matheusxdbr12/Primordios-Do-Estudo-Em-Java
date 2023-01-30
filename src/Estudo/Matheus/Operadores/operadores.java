3e69ba305c833f1b0587d132ed148ebe51a3d0a8
matheusxdbr12 <matheusxp21@gmail.com>
2023-01-30 08:36:22 -0300

    Op.Relacionais

--- src/Estudo/Matheus/Operadores/operadores.java
+++ src/Estudo/Matheus/Operadores/operadores.java
@@ -2,18 +2,41 @@ package Estudo.Matheus.Operadores;
 
 public class operadores {
     public static void main(String[] args) {
-        int a, b;
+        String nome1 = "Matheus";
+        String nome2 = new String("Matheus");
 
-         a = 6;
-         b = 6;
+        System.out.println(nome1.equals(nome2));
+             
+        int numero1 = 1;                   
+        int numero2 = 2;
 
-         String resultado = a==b ?"verdadeiro":"falso";
-         /*if(a==b)
-             resultado = "verdadeiro";
-         else
-             resultado = "falso";
-        */
-     System.out.println(resultado);                    
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            System.out.println("A nossa condição é verdadeira!");
+        }
+        
+        System.out.println("Número 1 é igual ao número 2? " + simNao);
+
+        simNao = numero1 != numero2;
+        
+        System.out.println("Número 1 é diferente ao número 2? " + simNao);
+
+        simNao = numero1 > numero2;
+        
+        System.out.println("Número 1 é maior que número 2? " + simNao);
+
+        simNao = numero1 < numero2;
+        
+        System.out.println("Número 1 é menor que número 2? " + simNao);
+
+        simNao = numero1 >= numero2;
+        
+        System.out.println("Número 1 é maior ou igual ao número 2? " + simNao);
+
+        simNao = numero1 <= numero2;
+        
+        System.out.println("Número 1 é menor ou igual ao número 2? " + simNao);
     }
     
 }
